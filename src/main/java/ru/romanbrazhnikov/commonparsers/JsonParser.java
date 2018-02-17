08a4251abdaf2e296aa842bfde825f834ee3b7fc
Roman <mail@romanbrazhnikov.ru>
2018-02-17 16:56:08 +0700

    JsonParser added (empty)

---
+++ src/main/java/ru/romanbrazhnikov/commonparsers/JsonParser.java
@@ -0,0 +1,33 @@
+package ru.romanbrazhnikov.commonparsers;
+
+import io.reactivex.Single;
+
+import java.util.Map;
+import java.util.Set;
+
+public class JsonParser implements ICommonParser {
+    @Override
+    public void setPattern(String pattern) {
+
+    }
+
+    @Override
+    public void setSource(String source) {
+
+    }
+
+    @Override
+    public void setMatchNames(Set<String> names) {
+
+    }
+
+    @Override
+    public void setBindings(Map<String, String> bindings) {
+
+    }
+
+    @Override
+    public Single<ParseResult> parse() {
+        return null;
+    }
+}
